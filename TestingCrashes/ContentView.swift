6530ffe6b42f8a4fa207ccc6261d44919c09f710
Mihai Arosoaie <marosoaie@gmail.com>
2020-06-01 21:39:44 +0200

    Initial Commit

---
+++ TestingCrashes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestingCrashes
+//
+//  Created by Mihai Arosoaie on 01/06/2020.
+//  Copyright © 2020 Mihai Arosoaie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
